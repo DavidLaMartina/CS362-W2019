48dc28174faa492fc3cb7f708ff5c86827fca9f6
David LaMartina <lamartid@oregonstate.edu>
2019-01-22 14:08:25 -0700

    Refactored functions, before introducing bugs:

--- projects/lamartid/dominion/dominion_helpers.h
+++ projects/lamartid/dominion/dominion_helpers.h
@@ -5,11 +5,29 @@
 
 int drawCard(int player, struct gameState *state);
 int updateCoins(int player, struct gameState *state, int bonus);
-int discardCard(int handPos, int currentPlayer, struct gameState *state, 
+int discardCard(int handPos, int currentPlayer, struct gameState *state,
 		int trashFlag);
 int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
-int cardEffect(int card, int choice1, int choice2, int choice3, 
+int cardEffect(int card, int choice1, int choice2, int choice3,
 	       struct gameState *state, int handPos, int *bonus);
+
+// Individual card-calling functions
+void smithy_play(
+	int currentPlayer, struct gameState *state, int handPos, int *bonus);
+
+void adventurer_play(
+	int currentPlayer, int drawntreasure, int z, int temphand[],
+	struct gameState *state, int handPos, int *bonus);
+
+void council_room_play(int currentPlayer, struct gameState *state, int handPos);
+
+int mine_play(
+	int currentPlayer, struct gameState *state, int handPos,
+	int choice1, int choice2);
+
+void steward_play(
+	int currentPlayer, struct gameState *state, int handPos,
+	int choice1, int choice2, int choice3);
 
 #endif
